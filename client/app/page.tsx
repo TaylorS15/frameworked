99a78e33e91d9ba8271c543b5157fbc8f3da8672
Taylor Svec <taylorsvec978@yahoo.com>
2024-01-17 00:16:15 -0500

    updated /public/ file structure, fixed css issues, cleaning

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -10,12 +10,19 @@ import * as api from "@/app/api";
 
 export default function Home() {
   const [code, setCode] = useState<string>("");
+  const [instructions, setInstructions] = useState<string>("");
+  const { width, height } = useWindowResize();
   const iframeRef = useRef<HTMLIFrameElement>(null);
 
   useEffect(() => {
-    fetch("/placeholder-code/react/1-Button-Click.txt")
+    fetch("/question-data/1-Counter/react/code.txt")
       .then((res) => res.text())
       .then((text) => setCode(text))
+      .catch((err) => console.error(err));
+
+    fetch("/question-data/1-Counter/react/instructions.txt")
+      .then((res) => res.text())
+      .then((text) => setInstructions(text))
       .catch((err) => console.error(err));
   }, []);
 
@@ -30,26 +37,25 @@ export default function Home() {
         URL.revokeObjectURL(url);
       }
     } catch (error) {
-      console.error("Error running code:", error);
+      console.error(error);
     }
   }
 
-  console.log("changing");
-
   return (
     <main className="flex min-h-screen w-screen flex-col items-center justify-center">
-      <ResizablePanelGroup direction="horizontal" className="h-[90vh] w-full">
-        <ResizablePanel
-          defaultSize={200}
-          className="max-h-[90vh] rounded-md border-2 border-purple-500"
-        >
+      <ResizablePanelGroup
+        direction={width < 1024 ? "vertical" : "horizontal"}
+        style={{ height: "90vh" }}
+        className="w-full border-2 border-orange-500"
+      >
+        <ResizablePanel className="w-full rounded-md border-2 border-purple-500">
           <h1>Instruction Panel</h1>
+          <p className="text-sm">{instructions}</p>
         </ResizablePanel>
+
         <ResizableHandle withHandle />
-        <ResizablePanel
-          defaultSize={200}
-          className="flex max-h-[90vh] flex-col gap-2"
-        >
+
+        <ResizablePanel className="flex flex-col gap-2">
           <Editor
             className="rounded-md border-2 border-blue-500"
             defaultLanguage="javascript"
@@ -57,7 +63,6 @@ export default function Home() {
             value={code}
             onChange={(e) => setCode(e ? e : "")}
           />
-
           <button
             className="w-24 rounded-md border-2 border-green-500"
             onClick={saveAndRun}
@@ -65,15 +70,39 @@ export default function Home() {
             Save & Run
           </button>
         </ResizablePanel>
+
         <ResizableHandle withHandle />
-        <ResizablePanel defaultSize={200} className="max-h-[90vh] w-max">
+
+        <ResizablePanel className="">
           <iframe
             sandbox="allow-scripts"
             ref={iframeRef}
-            className="h-full rounded-md border-2 border-red-500 bg-white"
+            className="h-full w-full rounded-md border-2 border-red-500 bg-white"
           />
         </ResizablePanel>
       </ResizablePanelGroup>
     </main>
   );
 }
+
+const useWindowResize = () => {
+  const [windowSize, setWindowSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return windowSize ?? { width: 1024, height: 1024 };
+};
