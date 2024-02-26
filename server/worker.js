6ac3b10f84df25fd1dec1a65078dd1541148fe72
Taylor Svec <taylorsvec978@yahoo.com>
2024-02-26 17:34:11 -0500

    improved filenaming comments

--- server/worker.js
+++ server/worker.js
@@ -8,20 +8,23 @@ const workerpool = require('workerpool');
 function transpile(code, css) {
 	return new Promise(async (resolve, reject) => {
 		const date = Date.now();
-		const uniqueFilename = `/temp_${date}_${Math.floor(date * Math.random())}.js`;
+		const uniqueFilename = `${date}_${Math.floor(date * Math.random())}`;
+		const tempFilename = `/temp_${uniqueFilename}.js`;
+		const bundleFilename = `/bundle_${uniqueFilename}.js`;
 
 		const memfsVol = createFsFromVolume(new Volume());
+		//Mirror the current dir in the memfs volume so that webpack can resolve node_modules
 		memfsVol.mkdirSync(__dirname, { recursive: true });
-		memfsVol.writeFileSync(path.join(__dirname, uniqueFilename), code);
+		memfsVol.writeFileSync(path.join(__dirname, tempFilename), code);
 
 		ufs.use(fs).use(memfsVol);
 
 		const compiler = webpack({
 			mode: 'production',
-			entry: uniqueFilename,
+			entry: tempFilename,
 			output: {
 				path: __dirname,
-				filename: 'bundle.js',
+				filename: bundleFilename.replace(/^\//, ''),
 			},
 			module: {
 				rules: [
@@ -38,6 +41,7 @@ function transpile(code, css) {
 			},
 		});
 
+		//Must use ufs as inputFileSystem insead of memfs because webpack must be able to resolve babel-loader/its presets from node_modules
 		compiler.inputFileSystem = ufs;
 		compiler.outputFileSystem = memfsVol;
 
@@ -48,11 +52,14 @@ function transpile(code, css) {
 				return;
 			}
 
-			const bundledCode = memfsVol.readFileSync(path.join(__dirname, 'bundle.js'), 'utf-8');
+			const bundledCode = memfsVol.readFileSync(
+				path.join(__dirname, bundleFilename),
+				'utf-8'
+			);
 
-			ufs.unlinkSync(path.join(__dirname, uniqueFilename));
-			ufs.unlinkSync(path.join(__dirname, 'bundle.js'));
-			ufs.unlinkSync(path.join(__dirname, 'bundle.js.LICENSE.txt'));
+			memfsVol.unlinkSync(path.join(__dirname, tempFilename));
+			memfsVol.unlinkSync(path.join(__dirname, bundleFilename));
+			memfsVol.unlinkSync(path.join(__dirname, `${bundleFilename}.LICENSE.txt`));
 
 			const htmlContent = `<html>\n\t<head>\n\t\t<style>${css}</style>\n\t</head>\n\t<body>\n\t\t<div id="root"></div>\n\t\t<script type="text/javascript">\n\t\t\t${bundledCode}\n\t\t</script>\n\t</body>\n</html>`;
 			resolve(htmlContent);
