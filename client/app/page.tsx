59d1c263df334ca33745f6d16e11fe6d023178f0
Taylor Svec <taylorsvec978@yahoo.com>
2024-03-07 18:10:18 -0500

    removed clerk auth

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,15 +1,9 @@
 import ChallengeList from "@/components/ChallengeList";
-import { SignUpButton, SignedOut, UserButton } from "@clerk/nextjs";
-import { ChevronRight } from "lucide-react";
 import Balancer from "react-wrap-balancer";
 
 export default async function Home() {
   return (
     <main className="max-w-screen flex min-h-screen flex-col items-center gap-8 bg-gradient-to-b from-blue-950/50 from-0% to-zinc-950 to-30%">
-      <div className="absolute right-4 top-4">
-        <UserButton />
-      </div>
-
       <div className="mt-24 flex flex-col gap-8">
         <h1 className="mx-auto bg-gradient-to-br from-white to-zinc-400 bg-clip-text text-5xl text-transparent sm:text-6xl md:text-7xl">
           Frame
@@ -20,19 +14,10 @@ export default async function Home() {
 
         <p className="mx-auto w-5/6 bg-gradient-to-br from-white to-zinc-400 bg-clip-text text-center text-xl font-semibold text-transparent">
           <Balancer>
-            Test your ability to answer relevant and challenging frontend
-            interview questions.
+            Test your ability to answer relevant and challenging frontend interview
+            questions.
           </Balancer>
         </p>
-
-        <SignedOut>
-          <SignUpButton>
-            <button className="mx-auto flex h-12 w-40 items-center justify-center gap-2 rounded-full border border-sky-900 bg-sky-700 font-semibold transition-all hover:gap-3 hover:bg-sky-600">
-              <p>Get started</p>
-              <ChevronRight className="inline-block" strokeWidth={1.5} />
-            </button>
-          </SignUpButton>
-        </SignedOut>
       </div>
 
       <ChallengeList className="mb-24 mt-24 h-[28rem] w-4/5 overflow-y-scroll rounded-md border border-zinc-600 lg:w-[42rem]" />
--- client/components/EditorPanel.tsx
+++ client/components/EditorPanel.tsx
@@ -1,5 +1,4 @@
 import { useStore } from "@/app/store";
-import { useClerk } from "@clerk/nextjs";
 import { Editor } from "@monaco-editor/react";
 import { useState } from "react";
 import * as api from "@/app/api";
@@ -19,7 +18,6 @@ export default function EditorPanel({
   } = useStore();
   const [isFetching, setIsFetching] = useState<boolean>(false);
   const [language, setLanguage] = useState<string>("javascript");
-  const clerk = useClerk();
 
   async function saveAndRunCode() {
     setIsFetching(true);
@@ -52,63 +50,58 @@ export default function EditorPanel({
 
   return (
     <>
-      {/*only checking if clerk is loaded because something is broken with monaco+clerk. There was a supposed fix pushed to clerk q4 2023, but still throws error. https://github.com/clerk/javascript/issues/1643 */}
-      {clerk.loaded && (
-        <>
-          <div className="flex h-10 min-h-10 w-full rounded-t-md bg-zinc-925">
-            {Object.keys(challengeFiles).map((file) => {
-              return (
-                <button
-                  key={file}
-                  disabled={!isTimerRunning}
-                  className={`${
-                    currentCode.fileName === file ? "bg-zinc-800" : "hover:bg-zinc-900"
-                  } h-full w-24 min-w-24 rounded-t-md transition-all ${
-                    isTimerRunning ? "" : "cursor-not-allowed disabled:opacity-50"
-                  }`}
-                  onClick={() => {
-                    setChallengeFiles({
-                      ...challengeFiles,
-                      [currentCode.fileName]: currentCode.code,
-                    });
-                    setCurrentCode(file, challengeFiles[file]);
-
-                    let splitLanguage = file.split("_")[1];
-                    splitLanguage === "js" && (splitLanguage = "javascript");
-                    setLanguage(splitLanguage);
-                  }}
-                >
-                  <p className="text-center text-xs">{file.replace("_", ".")}</p>
-                </button>
-              );
-            })}
-          </div>
-          <div className="relative h-editor-custom w-full">
-            <div
+      <div className="flex h-10 min-h-10 w-full rounded-t-md bg-zinc-925">
+        {Object.keys(challengeFiles).map((file) => {
+          return (
+            <button
+              key={file}
+              disabled={!isTimerRunning}
               className={`${
-                isTimerRunning ? "hidden" : ""
-              } absolute z-20 h-full w-full rounded-md bg-zinc-700/30 backdrop-blur-sm`}
-            />
-            <Editor
-              className="h-editor-custom rounded-b-md"
-              language={language}
-              theme="vs-dark"
-              value={currentCode.code}
-              options={{
-                minimap: { enabled: false },
-                wrappingIndent: "same",
-              }}
-              onChange={(e) => {
-                setCurrentCode(currentCode.fileName, e ? e : "");
+                currentCode.fileName === file ? "bg-zinc-800" : "hover:bg-zinc-900"
+              } h-full w-24 min-w-24 rounded-t-md transition-all ${
+                isTimerRunning ? "" : "cursor-not-allowed disabled:opacity-50"
+              }`}
+              onClick={() => {
                 setChallengeFiles({
                   ...challengeFiles,
                   [currentCode.fileName]: currentCode.code,
                 });
+                setCurrentCode(file, challengeFiles[file]);
+
+                let splitLanguage = file.split("_")[1];
+                splitLanguage === "js" && (splitLanguage = "javascript");
+                setLanguage(splitLanguage);
               }}
-            />
-          </div>
-        </>
-      )}
+            >
+              <p className="text-center text-xs">{file.replace("_", ".")}</p>
+            </button>
+          );
+        })}
+      </div>
+      <div className="relative h-editor-custom w-full">
+        <div
+          className={`${
+            isTimerRunning ? "hidden" : ""
+          } absolute z-20 h-full w-full rounded-md bg-zinc-700/30 backdrop-blur-sm`}
+        />
+        <Editor
+          className="h-editor-custom rounded-b-md"
+          language={language}
+          theme="vs-dark"
+          value={currentCode.code}
+          options={{
+            minimap: { enabled: false },
+            wrappingIndent: "same",
+          }}
+          onChange={(e) => {
+            setCurrentCode(currentCode.fileName, e ? e : "");
+            setChallengeFiles({
+              ...challengeFiles,
+              [currentCode.fileName]: currentCode.code,
+            });
+          }}
+        />
+      </div>
 
       <button
         className="mt-2 flex h-10 min-h-10 w-20 items-center justify-center rounded-md border transition-all hover:border-zinc-600 hover:bg-gradient-to-br hover:from-blue-900/50 hover:to-blue-900/20 disabled:cursor-not-allowed disabled:opacity-50"
