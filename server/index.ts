a316574c701617563a08f81b8bdddd4c586f0f1b
Taylor Svec <taylorsvec978@yahoo.com>
2024-03-07 17:26:17 -0500

    removed backend worker small frontend editor fixes

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import cors from 'cors';
 import path from 'path';
-import workerpool from 'workerpool';
-import { performance } from 'perf_hooks';
+import webpack from 'webpack';
+import fs from 'fs';
+import { createFsFromVolume, Volume } from 'memfs';
+import ufs, { IFS } from 'unionfs';
 
 const app = express();
 const port = 4000;
@@ -14,37 +16,78 @@ app.use(
 	})
 );
 
-const pool = workerpool.pool(path.resolve(__dirname, './worker.js'));
-
 app.post('/transpileReact', async (req, res) => {
+	const code =
+		"import React from 'react';\nimport ReactDOM from 'react-dom';\n" +
+		req.body.code +
+		"\n\nReactDOM.createRoot(document.getElementById('root')).render(<App />);";
+	const css = req.body.css;
+
 	try {
-		const t0 = performance.now();
-		const initialMemoryUsage = process.memoryUsage().heapUsed / 1024 / 1024;
-		const code =
-			"import React from 'react';\nimport ReactDOM from 'react-dom';\n" +
-			req.body.code +
-			"\n\nReactDOM.createRoot(document.getElementById('root')).render(<App />);";
-		const css = req.body.css;
+		//Creating unique filenames, not necessarily needed running locally.
+		const date = Date.now();
+		const uniqueFilename = `${date}_${Math.floor(date * Math.random())}`;
+		const tempFilename = `/temp_${uniqueFilename}.js`;
+		const bundleFilename = `/bundle_${uniqueFilename}.js`;
 
-		pool.exec('transpile', [code, css])
-			.then((result) => {
-				const t1 = performance.now();
-				const finalHeapUsed = process.memoryUsage().heapUsed / 1024 / 1024;
-				console.log('Transpilation took', (t1 - t0).toFixed(2), 'milliseconds');
-				console.log('Memory usage:', (finalHeapUsed - initialMemoryUsage).toFixed(2), 'MB');
+		const memfs = createFsFromVolume(new Volume());
+		//Mirror the current dir in the memfs volume so that webpack can resolve node_modules
+		memfs.mkdirSync(__dirname, { recursive: true });
+		memfs.writeFileSync(path.join(__dirname, tempFilename), code);
 
-				res.json({ transpiledReact: result });
-			})
-			.catch((err) => {
-				const t1 = performance.now();
-				const finalHeapUsed = process.memoryUsage().heapUsed / 1024 / 1024;
-				console.log('Transpilation took', t1 - t0, 'milliseconds');
-				console.log('Memory usage:', (finalHeapUsed - initialMemoryUsage).toFixed(2), 'MB');
+		/**
+		 * @TODO - Fix typing
+		 */
+		ufs.use(fs).use(memfs as unknown as IFS);
 
-				console.error(err);
-				res.status(500).send('Error transpiling code');
-			});
+		const compiler = webpack({
+			mode: 'production',
+			entry: tempFilename,
+			output: {
+				path: __dirname,
+				filename: bundleFilename.replace(/^\//, ''),
+			},
+			module: {
+				rules: [
+					{
+						test: /\.js$/,
+						use: {
+							loader: 'babel-loader',
+							options: {
+								presets: ['@babel/preset-env', '@babel/preset-react'],
+							},
+						},
+					},
+				],
+			},
+		});
+
+		//Must use ufs as inputFileSystem insead of memfs because webpack must be able to resolve babel-loader/its presets from node_modules
+		compiler.inputFileSystem = ufs;
+		compiler.outputFileSystem = memfs;
+
+		compiler.run((err, stats) => {
+			if (err) {
+				/**
+				 * @TODO - Handle errors
+				 */
+				console.log(err);
+				return;
+			}
+
+			const bundledCode = memfs.readFileSync(path.join(__dirname, bundleFilename), 'utf-8');
+
+			memfs.unlinkSync(path.join(__dirname, tempFilename));
+			memfs.unlinkSync(path.join(__dirname, bundleFilename));
+			memfs.unlinkSync(path.join(__dirname, `${bundleFilename}.LICENSE.txt`));
+
+			const htmlContent = `<html>\n\t<head>\n\t\t<style>${css}</style>\n\t</head>\n\t<body>\n\t\t<div id="root"></div>\n\t\t<script type="text/javascript">\n\t\t\t${bundledCode}\n\t\t</script>\n\t</body>\n</html>`;
+			res.json({ transpiledReact: htmlContent });
+		});
 	} catch (err) {
+		/**
+		 * @TODO - Handle errors
+		 */
 		console.error(err);
 		res.status(500).send('Error transpiling code');
 	}
